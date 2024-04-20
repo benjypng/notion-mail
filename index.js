9a4c05f85e0e6f40d71b1910c5eca6031eea6e2c
hkgnp <ben@pngs.cc>
2024-04-20 16:52:24 +0800

    feat: alternative strategy to filtering emails

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const mailOptions = {
   },
   tlsOptions: { rejectUnauthorized: false },
   mailbox: "INBOX", // mailbox to monitor
-  searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
+  searchFilter: ["SEEN", ["X-GM-LABELS", "notion-mail"]], // Filter based on your label name
   markSeen: true, // all fetched email willbe marked as seen and not fetched next time
   fetchUnreadOnStart: false, // use it only if you want to get all unread email on lib start. Default is `false`,
   attachments: false, // download attachments as they are encountered to the project directory
@@ -48,22 +48,23 @@ mailListener.on("server:disconnected", function () {
 mailListener.on("error", function (err) {
   console.log(err);
 });
-mailListener.on("mail", async function (mail, seqno) {
+mailListener.on("mail", async function (mail, _seqno) {
   // do something with the whole email as a single object
-  if (mail.subject.toLowerCase().startsWith("notionmail")) {
-    const subject = `${mail.subject
-      .replace("notionmail", "")
-      .replace("Notionmail", "")
-      .trim()}`;
+  const mailTime = Math.floor(Date.parse(mail.date) / 1000);
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (currentTime - mailTime > 10) return;
+
+  if (/(\b[nN][mM]\b)/.exec(mail.subject)) {
+    const subject = mail.subject.replace(/(\b[nN][mM]\b)/, "").trim();
     const text = mail.text;
 
     try {
-      const response = await notion.pages.create({
+      await notion.pages.create({
         parent: {
           database_id: process.env.DATABASE_ID,
         },
         properties: {
-          Title: {
+          Name: {
             type: "title",
             title: [
               {
@@ -73,10 +74,6 @@ mailListener.on("mail", async function (mail, seqno) {
                 },
               },
             ],
-          },
-          ["Inbox?"]: {
-            type: "checkbox",
-            checkbox: true,
           },
         },
         children: [
@@ -94,7 +91,6 @@ mailListener.on("mail", async function (mail, seqno) {
           },
         ],
       });
-      console.log(response);
       console.log("Email successfully sent to Notion");
     } catch (error) {
       console.error(error);
